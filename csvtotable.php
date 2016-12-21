a38628a582e6db03b3b8b82766f847abe0b63547
Raymond F Kowalski <rfk4@afsconnect1.njit.edu>
2016-12-21 16:10:42 -0500

    added simple error handling for project 2

--- csvtotable.php
+++ csvtotable.php
@@ -1,6 +1,12 @@
 <?php
 echo "<html><body><table>\n\n";
-$f = fopen("csvin.csv", "r");
+//simple error handling
+if(!file_exists("csvin.csv")) {
+  die("File not found");
+} else {
+  $f = fopen("csvin.csv", "r");
+}
+
 while (($line = fgetcsv($f)) !== false) 
 {
         echo "<tr>";
